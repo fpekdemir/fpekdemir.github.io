8b911d6f52947c95322c515deb2103573becd5c5
Fatih Pekdemir <hardiron@gmail.com>
2020-12-25 10:27:17 +0300

    Update reset_network.sh

--- reset_network.sh
+++ reset_network.sh
@@ -3,9 +3,7 @@
 #network_conn.sh 
 sudo ip addr flush dev eth0
 sudo /etc/init.d/networking start
-sudo /etc/init.d/network-manager start
 sudo ifconfig eth0 up
 sudo dhclient eth0
-sudo /etc/init.d/network-manager restart
 echo "IP:$(sudo ifconfig eth0 | grep "inet" | grep -v inet6 | cut -d ' ' -f10)"
 echo "Gateway:$(sudo route -n | grep -v Destination | grep UG | cut -d ' ' -f10)"
