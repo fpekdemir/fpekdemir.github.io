9f9576e58fd7e716dbd7b98a19caaf5a2c5e8394
Fatih Pekdemir <hardiron@gmail.com>
2020-12-25 10:23:36 +0300

    Create reset_network.sh
    parrot os (ubuntu) vmware network problem fixer

---
+++ reset_network.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#network_conn.sh 
+sudo ip addr flush dev eth0
+sudo /etc/init.d/networking start
+sudo /etc/init.d/network-manager start
+sudo ifconfig eth0 up
+sudo dhclient eth0
+sudo /etc/init.d/network-manager restart
+echo "IP:$(sudo ifconfig eth0 | grep "inet" | grep -v inet6 | cut -d ' ' -f10)"
+echo "Gateway:$(sudo route -n | grep -v Destination | grep UG | cut -d ' ' -f10)"
